4dbda64b164ae6b5b2a48f7e38567845cea2a128
ShomaTani <shomatani20070328@gmail.com>
2025-05-29 15:23:14 +0100

    First commit to new branch

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 import json
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 import os
 
-scope = ["https://spreadsheets.google.com/feeds", 
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=GEMINI_API_KEY)
+if not GEMINI_API_KEY:
+    raise RuntimeError("GEMINI_API_KEY not set in environment")
+print(os.environ.get("GEMINI_API_KEY"))
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets", 
          "https://www.googleapis.com/auth/drive"]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    'service_account.json', scope)
+creds = Credentials.from_service_account_file(
+    filename="service_account.json",
+    scopes=SCOPES
+)
+
 gc = gspread.authorize(creds)
 
-spreadsheet_id = '1gUXLRZQjD4M9a5w9m256CRtMAbyWNdr8psfOl_rplIs'
-sheet = gc.open('Q_A_test').sheet1
+spreadsheet_url = "https://docs.google.com/spreadsheets/d/1gUXLRZQjD4M9a5w9m256CRtMAbyWNdr8psfOl_rplIs/edit?gid=0#gid=0"
+sheet = gc.open_by_url(spreadsheet_url).sheet1
 rows = sheet.get_all_records()
 
 with open('questions.json', 'w', encoding='utf-8') as f:
@@ -22,10 +33,7 @@ with open('questions.json', 'w', encoding='utf-8') as f:
     
 print(rows)
 
-load_dotenv()
-client = genai.Client(api_key="GEMINI_API_KEY")
-
-def classify_and_summarise(test):
+def classify_and_summarize(test):
     prompt = f"""
     質問を分類して、類似質問を「○○に関する技術的質問」、「ステップ○○でのバグ報告」、「運用相談」などにまとめ、要約して以下の評価基準に則って優先度順に表示してください。
     -類似質問の数
@@ -36,23 +44,24 @@ def classify_and_summarise(test):
     
     質問：{test}
     出力形式はJSONで、
-    {
+    {{
         "分類": "分類名",
         "類似質問数": 数字,
         "講師回答の必要性": "高/中/低",
         "講義との関連性": "高/中/低",
         "一般性": "高/中/低",
         "要約": "要約内容"
-    }
+    }}
     """
     response = client.models.generate_content(
         model="gemini-2.0-flash",
-        contents=[{"role":"user", "content": prompt}],
+        contents=[prompt],
         config=types.GenerateContentConfig(
             max_output_tokens=500,
             temperature=0.1
         )
     )
-    return response.choices[0].message.content
-    
-print(classify_and_summarise(rows))
+    return json.loads(response.text)
+
+for rec in rows:
+    print(classify_and_summarize(rec["質問"]))
